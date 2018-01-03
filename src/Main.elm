b335591c2b14db4f9ffdc9c632d8d6cf01206cef
Ashton Hellwig <hellwigashton@gmail.com>
2018-01-03 03:59:19 -0700

    add header

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ import Html exposing (..)
 import Html.Attributes exposing (class, href, style)
 import Material
 import Material.Button as Button
+import Material.Layout as Layout
 import Material.Options as Options exposing (css)
 import Material.Scheme
 
@@ -78,28 +79,22 @@ type alias Mdl =
 
 view : Model -> Html Msg
 view model =
+    Layout.render Mdl
+        model.mdl
+        [ Layout.fixedHeader
+        ]
+        { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "Counter" ] ]
+        , drawer = []
+        , tabs = ( [], [] )
+        , main = [ viewBody model ]
+        }
+
+
+viewBody : Model -> Html Msg
+viewBody model =
     div
         [ style [ ( "padding", "2rem" ) ] ]
         [ text ("Current count: " ++ toString model.count)
-
-        {- We construct the instances of the Button component that we need, one
-           for the increase button, one for the reset button. First, the increase
-           button. The first three arguments are:
-
-             - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-             - An instance id (the `[0]`). Every component that uses the same model
-               collection (model.mdl in this file) must have a distinct instance id.
-             - A reference to the elm-mdl model collection (`model.mdl`).
-
-           Notice that we do not have to add fields for the increase and reset buttons
-           separately to our model; and we did not have to add to our update messages
-           to handle their internal events.
-
-           Mdl components are configured with `Options`, similar to `Html.Attributes`.
-           The `Options.onClick Increase` option instructs the button to send the `Increase`
-           message when clicked. The `css ...` option adds CSS styling to the button.
-           See `Material.Options` for details on options.
-        -}
         , Button.render Mdl
             [ 0 ]
             model.mdl
